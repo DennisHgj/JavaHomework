07d0232535e365761929635330ff8062f2b194f3
Dennis <7401444281@qq.com>
2018-07-26 17:33:30 +1000

    finished the homework J1-J3,J4.Name

--- src/comp1110/homework/J04/Name.java
+++ src/comp1110/homework/J04/Name.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 
 public class Name {
     public static void main(String[] args) {
-
-
+        Scanner in = new Scanner(System.in);
+        String Name= in.nextLine();
+        int a = Name.length();
+        System.out.println(Name.toUpperCase());
+        System.out.println(a);
     }
 }
