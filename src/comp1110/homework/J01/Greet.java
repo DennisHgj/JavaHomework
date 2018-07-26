22e304fb19ccc0a17a17dc91d215e3e8cce09e0b
Dennis <7401444281@qq.com>
2018-07-26 16:40:16 +1000

    finished the homework J1-J3

---
+++ src/comp1110/homework/J01/Greet.java
@@ -0,0 +1,10 @@
+package comp1110.homework.J01;
+import java.util.Scanner;
+
+public class Greet {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String Greeting =in.next();
+        System.out.println("Hi " + Greeting+ "!");
+    }
+}
