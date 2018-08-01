b83fbe256577a8257abead2fb575f125c3b076b7
Dennis <7401444281@qq.com>
2018-08-02 09:26:09 +1000

    have some problems in j06

---
+++ src/comp1110/homework/J04/test.java
@@ -0,0 +1,8 @@
+package comp1110.homework.J04;
+
+public class test {
+    public static void main(String[] args) {
+        String a = "1234";
+        System.out.println(a.substring(2,3));
+    }
+}
