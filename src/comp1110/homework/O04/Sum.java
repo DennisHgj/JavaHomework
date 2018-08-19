4784c69ea086ecac4ac76a458ad0d7e6afc2f557
Dennis <7401444281@qq.com>
2018-08-19 20:04:05 +1000

    finished homework J08 J9,O4 Shape,and passed all tests.

--- src/comp1110/homework/O04/Sum.java
+++ src/comp1110/homework/O04/Sum.java
@@ -1,4 +1,61 @@
 package comp1110.homework.O04;
 
-public class Sum {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Sum extends ArrayList<Integer> {
+    private Integer elements;
+    private Integer size;
+
+    Sum(Integer elements,Integer size) {
+        this.elements = elements;
+        this.size= size;
+    }
+
+    List<Integer> list = new ArrayList<Integer>();
+
+    public int sum() {
+        int sum = 0;
+        list.add(this.elements);
+        this.size=list.size();
+        for (int i = 0; i < list.size(); i++) {
+            sum = sum + list.get(i);
+        }
+
+
+        return sum;
+    }
+
+    @Override
+    public boolean add(Integer i) {
+        this.elements=i;
+        sum();
+        return true;
+    }
+
+
+    @Override
+    public void add(int index, Integer i) {
+        for (int s =0;s<list.size();s++){
+
+        }
+
+
+
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Integer> c) {
+        return true;
+
+
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Integer> c){
+        return true;
+    }
+
+
 }
